2e65f7cac085941b7596adb226f0fc2a6d466a3b
Sergei SMART <sergei.smart@gmail.com>
2022-03-14 19:42:36 +1000

    upload lib files

---
+++ lib/BaseTree.d.ts
@@ -0,0 +1,42 @@
+import { Element, HashFunction, ProofPath } from './index';
+export declare class BaseTree {
+    levels: number;
+    protected _hashFn: HashFunction<Element>;
+    protected zeroElement: Element;
+    protected _zeros: Element[];
+    protected _layers: Array<Element[]>;
+    get capacity(): number;
+    get layers(): Array<Element[]>;
+    get zeros(): Element[];
+    get elements(): Element[];
+    get root(): Element;
+    /**
+     * Find an element in the tree
+     * @param element An element to find
+     * @param comparator A function that checks leaf value equality
+     * @returns {number} Index if element is found, otherwise -1
+     */
+    indexOf(element: Element, comparator?: <T>(arg0: T, arg1: T) => boolean): number;
+    /**
+     * Insert new element into the tree
+     * @param element Element to insert
+     */
+    insert(element: Element): void;
+    bulkInsert(elements: Element[]): void;
+    /**
+     * Change an element in the tree
+     * @param {number} index Index of element to change
+     * @param element Updated element value
+     */
+    update(index: number, element: Element): void;
+    proof(element: Element): ProofPath;
+    /**
+     * Get merkle path to a leaf
+     * @param {number} index Leaf index to generate path for
+     * @returns {{pathElements: Object[], pathIndex: number[]}} An object containing adjacent elements and left-right index
+     */
+    path(index: number): ProofPath;
+    protected _buildZeros(): void;
+    protected _processNodes(nodes: Element[], layerIndex: number): any[];
+    protected _processUpdate(index: number): void;
+}
---
+++ lib/index.d.ts
@@ -0,0 +1,44 @@
+export { default as MerkleTree } from './FixedMerkleTree';
+export { PartialMerkleTree } from './PartialMerkleTree';
+export { simpleHash } from './simpleHash';
+export declare type HashFunction<T> = {
+    (left: T, right: T): string;
+};
+export declare type MerkleTreeOptions = {
+    hashFunction?: HashFunction<Element>;
+    zeroElement?: Element;
+};
+export declare type Element = string | number;
+export declare type SerializedTreeState = {
+    levels: number;
+    _zeros: Array<Element>;
+    _layers: Array<Element[]>;
+};
+export declare type SerializedPartialTreeState = {
+    levels: number;
+    leaves: Element[];
+    _edgeElementsCount: number;
+    _zeros: Array<Element>;
+    _edgeLeafProof: ProofPath;
+    _edgeLeaf: LeafWithIndex;
+};
+export declare type ProofPath = {
+    pathElements: Element[];
+    pathIndices: number[];
+    pathPositions: number[];
+    pathRoot: Element;
+};
+export declare type TreeEdge = {
+    edgeElement: Element;
+    edgePath: ProofPath;
+    edgeIndex: number;
+    edgeElementsCount: number;
+};
+export declare type TreeSlice = {
+    edge: TreeEdge;
+    elements: Element[];
+};
+export declare type LeafWithIndex = {
+    index: number;
+    data: Element;
+};
